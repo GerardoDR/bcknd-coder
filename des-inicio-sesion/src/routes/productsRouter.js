ad1a2ee4fb0105b5cbea240d7c161797ac99cc89
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2022-06-29 22:19:11 -0300

    inicio sesion listo

---
+++ des-inicio-sesion/src/routes/productsRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const ProductsDaoMongo = require("../containers/ProductsDaoMongo");
+
+const productsRouter = express.Router();
+
+let productsContainer = new ProductsDaoMongo();
+
+// const userAdmin = true;
+
+productsRouter.get('/', async (req, res) => {
+  let products = await productsContainer.getAll()
+  res.json({products:products})
+});
+
+productsRouter.post('/', async (req, res) => {
+  let product = req.body;
+
+  if (product && product.title && product.price) {
+      let newProduct = await productsContainer.save(product)
+      res.json({result: 'product saved', product: newProduct[0]});
+  } else {
+      res.json({result: 'product cannot be saved', error: true});
+  }
+});
+
+module.exports = productsRouter;
